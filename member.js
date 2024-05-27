7b9cfe4b3e6abe5dad37da48bab1fb3cacab69dd
himanshigarg <himanshigarg1312@gmail.com>
2024-05-27 12:37:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,44 @@
+function skillsMember() {
+  return [
+    {
+      name: "HTML",
+      level: 70,
+    },
+    {
+      name: "CSS",
+      level: 60,
+    },
+    {
+      name: "JavaScript",
+      level: 80,
+    },
+    {
+      name: "React",
+      level: 70,
+    },
+    {
+      name: "Vue",
+      level: 60,
+    },
+    {
+      name: "Node",
+      level: 50,
+    },
+    {
+      name: "Python",
+      level: 40,
+    },
+    {
+      name: "Java",
+      level: 30,
+    },
+    {
+      name: "C",
+      level: 20,
+    },
+    {
+      name: "C++",
+      level: 10,
+    },
+  ];
+}
